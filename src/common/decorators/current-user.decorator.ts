30d3581e1229138227c3c13bbd78e6fe21018d54
ErikSantiago-G <Sg1861111@gmail.com>
2025-10-07 12:30:29 -0500

    The current user decoration is created, and it is also documented with JS Doc style

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+/**
+ * @decorator CurrentUser
+ * 
+ * Este decorador extrae el objeto `user` del objeto `request`, el cual
+ * normalmente es añadido por un guard de autenticación
+ *
+ * @param {ExecutionContext} context - Contexto de ejecución de NestJS que permite acceder al request HTTP.
+ * @returns {JwtPayload} El usuario autenticado contenido en `request.user`.
+ */
+export const CurrentUser = createParamDecorator((context: ExecutionContext) => {
+  const request = context.switchToHttp().getRequest();
+  return request.user;
+});
