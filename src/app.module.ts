9ddb464586cfec686f5ae1e0f67ae9857f3cc853
ErikSantiago-G <Sg1861111@gmail.com>
2025-10-07 11:56:04 -0500

    Auth, JWT token, and refresh token guards are performed.

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { ThrottlerModule } from "@nestjs/throttler";
+import { PrismaModule } from "./prisma/prisma.module";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { ProductsModule } from "./products/products.module";
+import { CategoriesModule } from "./categories/categories.module";
+import { CartModule } from "./cart/cart.module";
+import { OrdersModule } from "./orders/orders.module";
+import { CmsModule } from "./cms/cms.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    CartModule,
+    OrdersModule,
+    CmsModule,
+  ],
+})
+export class AppModule {}
